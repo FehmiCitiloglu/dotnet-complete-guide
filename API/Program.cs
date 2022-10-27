7d6dc3c9055d7f6de8187620c364474b7ce7482c
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-10-27 19:22:29 +0300

    create activities controller

--- API/Program.cs
+++ API/Program.cs
@@ -13,7 +13,7 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
             using var scope = host.Services.CreateScope();
@@ -21,7 +21,8 @@ namespace API
             try
             {
                 var context = services.GetRequiredService<DataContext>();
-                context.Database.Migrate();
+                await context.Database.MigrateAsync();
+                await Seed.SeedData(context);
             }
             catch (Exception ex)
             {
@@ -29,7 +30,7 @@ namespace API
                 logger.LogError(ex, "An error occurred during migration");
             }
 
-            host.Run();
+            await host.RunAsync();
         }
 
 
